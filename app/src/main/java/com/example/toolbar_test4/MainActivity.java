8d8e1022216edb2e5af16f494563da4c71a36ee8
tharun <vxt71780@gmail.com>
2020-05-07 14:03:09 -0500

    added UI

---
+++ app/src/main/java/com/example/toolbar_test4/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.toolbar_test4;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()){
+            case R.id.action_menu1:
+                startActivity(new Intent(this,Main2Activity.class));
+                return true;
+            case R.id.action_menu2:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+            case R.id.action_menu3:
+                startActivity(new Intent(this,Main3Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
+
+
+}
