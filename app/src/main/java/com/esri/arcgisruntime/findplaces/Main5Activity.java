8e65d54f1ceddd66e19f914af2d2e4c8bc00dbb6
tharun <vxt71780@gmail.com>
2020-05-11 21:36:58 -0500

    final changes

---
+++ app/src/main/java/com/esri/arcgisruntime/findplaces/Main5Activity.java
@@ -0,0 +1,279 @@
+package com.esri.arcgisruntime.findplaces;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.loadable.LoadStatusChangedEvent;
+import com.esri.arcgisruntime.loadable.LoadStatusChangedListener;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Callout;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.DrawStatus;
+import com.esri.arcgisruntime.mapping.view.DrawStatusChangedEvent;
+import com.esri.arcgisruntime.mapping.view.DrawStatusChangedListener;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.IdentifyGraphicsOverlayResult;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.NavigationChangedEvent;
+import com.esri.arcgisruntime.mapping.view.NavigationChangedListener;
+import com.esri.arcgisruntime.mapping.view.ViewpointChangedEvent;
+import com.esri.arcgisruntime.mapping.view.ViewpointChangedListener;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.tasks.geocode.GeocodeParameters;
+import com.esri.arcgisruntime.tasks.geocode.GeocodeResult;
+import com.esri.arcgisruntime.tasks.geocode.LocatorTask;
+import com.esri.arcgisruntime.util.ListenableList;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.esri.arcgisruntime.security.AuthenticationManager.CredentialCache.clear;
+
+public class Main5Activity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private GraphicsOverlay graphicsOverlay;
+    private LocatorTask locator = new LocatorTask("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+    private Spinner spinner;
+
+    // setup a new map at your area of interest.
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 28.0339;
+            double longitude = 1.6596;
+            int levelOfDetail = 5;
+            final ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+
+            // wait for the map to be fully ready and rendered before allowing a place search
+            mMapView.addViewpointChangedListener(new ViewpointChangedListener() {
+                @Override
+                public void viewpointChanged(ViewpointChangedEvent viewpointChangedEvent) {
+                    if (graphicsOverlay == null) {
+                        // Create a graphics overlay that will show the places found from a search.
+                        graphicsOverlay = new GraphicsOverlay();
+                        mMapView.getGraphicsOverlays().add(graphicsOverlay);
+                        setupSpinner();
+                        setupPlaceTouchListener();
+                        setupNavigationChangedListener();
+                        mMapView.removeViewpointChangedListener(this);
+                    }
+                }
+            });
+        }
+    }
+
+    // The Spinner shows the possible categories to search for. When the user selects a
+    // category call findPlaces with the category.
+    private void setupSpinner() {
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                findPlaces(adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        findPlaces(spinner.getSelectedItem().toString());
+    }
+
+    // Provide additional information about a specific place if the user taps that graphic.
+    private void setupPlaceTouchListener() {
+        mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+
+                // Dismiss a prior callout.
+                mMapView.getCallout().dismiss();
+
+                // get the screen point where user tapped
+                final android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()), Math.round(motionEvent.getY()));
+
+                // identify graphics on the graphics overlay
+                final ListenableFuture<IdentifyGraphicsOverlayResult> identifyGraphic = mMapView.identifyGraphicsOverlayAsync(graphicsOverlay, screenPoint, 10.0, false, 2);
+
+                identifyGraphic.addDoneListener(() -> {
+                    try {
+                        IdentifyGraphicsOverlayResult graphicsResult = identifyGraphic.get();
+                        // get the list of graphics returned by identify graphic overlay
+                        List<Graphic> graphicList = graphicsResult.getGraphics();
+
+                        // get the first graphic selected and show its attributes with a callout
+                        if (!graphicList.isEmpty()){
+                            showCalloutAtLocation(graphicList.get(0), mMapView.screenToLocation(screenPoint));
+                        }
+                    } catch (InterruptedException | ExecutionException exception) {
+                        exception.printStackTrace();
+                    }
+                });
+                return super.onSingleTapConfirmed(motionEvent);
+            }
+        });
+    }
+
+    private void setupNavigationChangedListener() {
+        mMapView.addNavigationChangedListener(navigationChangedEvent -> {
+            if (!navigationChangedEvent.isNavigating()) {
+                // Dismiss a prior callout before doing a new search.
+                mMapView.getCallout().dismiss();
+                findPlaces(spinner.getSelectedItem().toString());
+            }
+        });
+    }
+
+    // Show the selected graphic with its attributes in a callout
+    private void showCalloutAtLocation(Graphic graphic, Point mapPoint) {
+        Callout callout = mMapView.getCallout();
+        TextView calloutContent = new TextView(getApplicationContext());
+
+        callout.setLocation(graphic.computeCalloutLocation(mapPoint, mMapView));
+        calloutContent.setTextColor(Color.BLACK);
+        calloutContent.setText(Html.fromHtml("<b>" + graphic.getAttributes().get("PlaceName").toString() + "</b><br>" + graphic.getAttributes().get("Place_addr").toString()));
+        callout.setContent(calloutContent);
+        callout.show();
+    }
+
+    // Find places of the requested category within 50 kilometers of the center of the current map view.
+    private void findPlaces(String placeCategory) {
+        GeocodeParameters parameters = new GeocodeParameters();
+        Point searchPoint;
+
+        // Set the current map extent as the current location. The search is limited to a
+        // radius of 50 kilometers around this point.
+        if (mMapView.getVisibleArea() != null) {
+            searchPoint = mMapView.getVisibleArea().getExtent().getCenter();
+            if (searchPoint == null) {
+                return;
+            }
+        } else {
+            return;
+        }
+        parameters.setPreferredSearchLocation(searchPoint);
+
+        // We're interested in the top 25 nearest places.
+        parameters.setMaxResults(25);
+
+        // Return the place name and address fields in the results.
+        List<String> outputAttributes = parameters.getResultAttributeNames();
+        outputAttributes.add("Place_addr");
+        outputAttributes.add("PlaceName");
+
+        // Execute the search and add the places to the graphics overlay.
+        final ListenableFuture<List<GeocodeResult>> results = locator.geocodeAsync(placeCategory, parameters);
+        results.addDoneListener(() -> {
+            try {
+                ListenableList<Graphic> graphics = graphicsOverlay.getGraphics();
+                graphics.clear();
+                List<GeocodeResult> places = results.get();
+                for (GeocodeResult result : places) {
+
+                    // Add a graphic representing each location with a simple marker symbol.
+                    SimpleMarkerSymbol placeSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.GREEN, 10);
+                    placeSymbol.setOutline(new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.WHITE, 2));
+                    Graphic graphic = new Graphic(result.getDisplayLocation(), placeSymbol);
+                    java.util.Map<String, Object> attributes = result.getAttributes();
+
+                    // Store the location attributes with the graphic for later recall when this location is identified.
+                    for (String key : attributes.keySet()) {
+                        String value = attributes.get(key).toString();
+                        graphic.getAttributes().put(key, value);
+                    }
+                    graphics.add(graphic);
+                }
+            } catch (InterruptedException | ExecutionException exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()){
+            case R.id.action_menu1:
+                startActivity(new Intent(this,Main2Activity.class));
+                return true;
+            case R.id.action_menu2:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+            case R.id.subitem1:
+                startActivity(new Intent(this,Main6Activity.class));
+                return true;
+            case R.id.subitem2:
+                startActivity(new Intent(this,Main5Activity.class));
+                return true;
+            case R.id.subitem3:
+                startActivity(new Intent(this,Main7Activity.class));
+                return true;
+            case R.id.action_menu3:
+                startActivity(new Intent(this,Main3Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/esri/arcgisruntime/findplaces/Main7Activity.java
@@ -0,0 +1,68 @@
+package com.esri.arcgisruntime.findplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Main7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView text =(TextView) findViewById(R.id.editText4);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()){
+            case R.id.action_menu1:
+                startActivity(new Intent(this,Main2Activity.class));
+                return true;
+            case R.id.action_menu2:
+                startActivity(new Intent(this,MainActivity.class));
+                return true;
+            case R.id.subitem1:
+                startActivity(new Intent(this,Main6Activity.class));
+                return true;
+            case R.id.subitem2:
+                startActivity(new Intent(this,Main5Activity.class));
+                return true;
+            case R.id.subitem3:
+                startActivity(new Intent(this,Main7Activity.class));
+                return true;
+
+            case R.id.action_menu3:
+                startActivity(new Intent(this,Main3Activity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+}
